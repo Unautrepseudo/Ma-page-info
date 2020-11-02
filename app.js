423267fdca605bb4bb992f85265b353eb54d55af
Eric <eric.narbo@gmail.com>
2020-11-02 16:24:46 +0100

    fetch

--- app.js
+++ app.js
@@ -1,58 +1,73 @@
-// let ap = document.querySelector('#ap');
-// let nameTable =[];
+ fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(response => response.json())
+    .then((data) =>{
+        let output ='';
+     data.forEach(function(test){
+          
+       output +=
+        `
+        <div>
+            ${test.title}
+        </div>
+        <div>
+            ${test.body}
+        </div>
+        `
+
+        ap.innerHTML = output
+    })
+})
+
+
+
+
+
+
+
+ let ap = document.querySelector('#ap');
+ let ap2 = document.querySelector('#ap2');
+
+// let search = document.querySelector('#search');
+// let array =[];
+// let requestURL = 'https://jsonplaceholder.typicode.com/posts';
 // let request = new XMLHttpRequest();
-// request.onreadystatechange = function (){
-//     if(this.readyState === 4 && this.status === 200){
-//         let response = JSON.parse(this.responseText);
-//         response.forEach( resp => nameTable += [resp.name]);
-//         ap.innerHTML = nameTable;
-
-//         }
-//         console.log(nameTable)
-//     }
-
-// request.open('GET','https://jsonplaceholder.typicode.com/users');
+// let noms =[];
+// let contenu = [];
+// request.open('GET', requestURL);
+// request.responseType ='json';
 // request.send();
 
+// request.onload = function(){
+//     let test = request.response;
+//     names(test);
+//     toto(test);
+// }
 
-
-let ap = document.querySelector('#ap');
-let search = document.querySelector('#search');
-let array =[];
-let requestURL = 'https://jsonplaceholder.typicode.com/posts';
-let request = new XMLHttpRequest();
-let noms =[];
-
-request.open('GET', requestURL);
-request.responseType ='json';
-request.send();
-
-request.onload = function(){
-    let test = request.response;
-    names(test);
+// function names(jsonObj){
     
-    //console.log(test)
-}
-
-function names(jsonObj){
-    
-    for( i =0; i<jsonObj.length; i++){
-       // ap.innerHTML += `<li> ${jsonObj[i].title} </li>`;
-        array.push(jsonObj[i].title)
-        //noms =jsonObj[i].title;
+//     for( i =0; i<jsonObj.length; i++){
+//         array.push(jsonObj[i].title);
+//         contenu.push(jsonObj[i].body)        
         
-    }
-}
-console.log(array)
+//     }
+// }
+// console.log(contenu)
+// //auto-completion
+// search.addEventListener('input',function(e){
 
-search.addEventListener('input',function(e){
+//     if(e.target.value){
+//        noms = array.filter( nom => nom.toLowerCase().startsWith(e.target.value));
+//        noms = noms.map(nom =>`<li class='result-title'> ${nom} </li>`).join('')
+//        ap.innerHTML = noms;
+//        let eli = document.querySelectorAll('.result-title');
+//        eli.forEach( el => el.addEventListener('click', toto));
+//     }else{
+//         ap.innerHTML = '';
 
-    if(e.target.value){
-       noms = array.filter( nom => nom.toLowerCase().startsWith(e.target.value));
-       noms = noms.map(nom =>`<li class='test'> ${nom} </li>`)
-       ap.innerHTML = noms.join('');
-    }else{
-        ap.innerHTML = '';
+//     }
+// })
 
-    }
-})
+// function toto(){
+//    console.log(this.innerText)
+//    content = contenu.filter( cont =>cont)
+// }

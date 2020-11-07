d7d9dc5bd5f66b6d82364522006aefd798c4f109
Eric <eric.narbo@gmail.com>
2020-11-07 01:09:41 +0100

    css hover into mouseEvent JS

--- app.js
+++ app.js
@@ -119,16 +119,156 @@
 
 
 
-const themBloc = document.querySelector('.yuyu');
-const flip = document.querySelector('.nav-flip');
-const flap = document.querySelector('.nav-flap');
-themBloc.addEventListener('mouseover',function(){
-    flip.style.opacity = 0;
-    flap.style.opacity = 1;
+// const themBloc = document.querySelector('.yuyu');
+// const flip = document.querySelector('.nav-flip');
+// const flap = document.querySelector('.nav-flap');
+// const nav = document.querySelector('.nav');
+// themBloc.addEventListener('mouseover',function(){
+//     flip.style.opacity = 0;
+//     flap.style.opacity = 1;
+//     flap.style.background = 'linear-gradient(149deg, rgba(58,96,180,0) 0%, rgba(29,175,253,0) 82%, rgba(69,168,252,0.783650078369906) 100%)';
+//     nav.style.borderBottom ='1px solid rgba(8, 177, 163, 0.916)';
+// })
+// themBloc.addEventListener('mouseout',function(){
+//     flip.style.opacity = 1;
+//     flap.style.opacity = 0;
+//     nav.style.borderBottom ='1px solid rgba(8, 177, 163, 0.116)';
 
-})
-themBloc.addEventListener('mouseout',function(){
-    flip.style.opacity = 1;
-    flap.style.opacity = 0;
+// })
+const themesTable =[
+    {
+        name : 'Science',
+        icone: `<i class="fas fa-microscope"></i>`,
+        borderL :'1px solid rgba(15, 131, 151, 0.205)',
+        borderR:'1px solid rgba(15, 131, 151, 0.281)',
+        boxShad: '1px 1px 4px rgba(8, 177, 163, 0.219)',
+        lineColor : '1px solid rgba(28, 177, 163, 0.916)',
+        bckGround : 'linear-gradient(25deg, rgba(69,168,252,0) 0%, rgba(255,255,255,0) 67%, rgba(33,181,38,0.783650078369906) 100%)'
 
-})
+    },
+    {
+        name : 'Economie',
+        icone: `<i class="fas fa-piggy-bank"></i>`,
+        borderL :'1px solid rgba(15, 131, 151, 0.205)',
+        borderR:'1px solid rgba(15, 131, 151, 0.281)',
+        boxShad: '1px 1px 4px rgba(8, 177, 163, 0.219)',
+        lineColor : '1px solid rgba(28, 177, 163, 0.916)',
+        bckGround : 'linear-gradient(25deg, rgba(69,168,252,0) 0%, rgba(255,255,255,0) 67%, rgba(33,181,38,0.783650078369906) 100%)'
+
+    },
+
+    {
+        name : 'France',
+        icone: `<i class="fas fa-frog"></i>`,
+        borderL :'1px solid rgba(15, 131, 151, 0.205)',
+        borderR:'1px solid rgba(15, 131, 151, 0.281)',
+        boxShad: '1px 1px 4px rgba(8, 177, 163, 0.219)',
+        lineColor : '1px solid rgba(28, 177, 163, 0.916)',
+        bckGround : 'linear-gradient(25deg, rgba(69,168,252,0) 0%, rgba(255,255,255,0) 67%, rgba(33,181,38,0.783650078369906) 100%)'
+
+    },
+    {
+        name : 'Europe',
+        icone: `<i class="fas fa-euro-sign"></i>`,
+        borderL :'1px solid rgba(15, 131, 151, 0.205)',
+        borderR:'1px solid rgba(15, 131, 151, 0.281)',
+        boxShad: '1px 1px 4px rgba(8, 177, 163, 0.219)',
+        lineColor : '1px solid rgba(28, 177, 163, 0.916)',
+        bckGround : 'linear-gradient(25deg, rgba(69,168,252,0) 0%, rgba(255,255,255,0) 67%, rgba(33,181,38,0.783650078369906) 100%)'
+
+    },
+    {
+        name : 'Monde',
+        icone: `<i class="fas fa-globe"></i>`,
+        borderL :'1px solid rgba(15, 131, 151, 0.205)',
+        borderR:'1px solid rgba(15, 131, 151, 0.281)',
+        boxShad: '1px 1px 4px rgba(8, 177, 163, 0.219)',
+        lineColor : '1px solid rgba(28, 177, 163, 0.916)',
+        bckGround : 'linear-gradient(25deg, rgba(69,168,252,0) 0%, rgba(255,255,255,0) 67%, rgba(33,181,38,0.783650078369906) 100%)'
+
+    },
+    {
+        name : 'Météo',
+        icone: `<i class="fas fa-cloud-sun-rain"></i>`,
+        borderL :'1px solid rgba(15, 131, 151, 0.205)',
+        borderR:'1px solid rgba(15, 131, 151, 0.281)',
+        boxShad: '1px 1px 4px rgba(8, 177, 163, 0.219)',
+        lineColor : '1px solid rgba(28, 177, 163, 0.916)',
+        bckGround : 'linear-gradient(25deg, rgba(69,168,252,0) 0%, rgba(255,255,255,0) 67%, rgba(33,181,38,0.783650078369906) 100%)'
+
+    }
+]
+
+
+const themeContainer = document.querySelector('.themes-container');
+function showNavItems (){
+    themesTable.forEach( elem =>
+    {
+        themeContainer.innerHTML += 
+        `   
+            <div class="theme-bloc yaya">
+                <div class='nav-icone'>${elem.icone}</div>
+                <div class=' nav-texte '>${elem.name}</div>
+            </div>
+        `;
+    })
+    const navThemes= document.querySelectorAll('.theme-bloc');
+    for (i=0; i< navThemes.length;i++){
+
+        navThemes[i].addEventListener('mouseover',function(){
+            let navIcon= this.querySelector('.nav-icone')
+            let navText= this.querySelector('.nav-texte')
+
+        navIcon.style.opacity = 0;
+        navText.style.opacity = 1;
+        })
+
+        navThemes[i].addEventListener('mouseout',function(){
+            let navIcon= this.querySelector('.nav-icone')
+            let navText= this.querySelector('.nav-texte')
+        navIcon.style.opacity = 1;
+        navText.style.opacity = 0;
+       
+
+        })
+    }
+}
+showNavItems()
+
+function toggleContent(){
+    
+}
+
+// const navIcon= document.querySelectorAll('.nav-icone');
+// const navText= document.querySelectorAll('.nav-texte');
+// const nav = document.querySelector('.nav')
+
+// for (i=0; i< navThemes.length;i++){
+//     navThemes[i].addEventListener('mouseover',function(){
+//         console.log(this.name)
+
+//         // navIcon[i].style.opacity = 0;
+//         // navText[i].style.opacity = 1;
+    
+//         // this.style.boxShadow = table[i].boxShad;
+//         // this.style.background = table[i].bckGround;
+//         // this.style.borderLeft = table[i].borderL;
+//         // this.style.borderRight = table[i].borderR;
+//         // nav.style.borderBottom = table[i].lineColor;
+//     })
+
+
+    // navThemes[i].addEventListener('mouseout',function(){
+    //     navIcon[i].style.opacity = 1;
+    //     navText[i].style.opacity = 0;
+    //     nav.style.borderBottom ='1px solid rgba(8, 177, 163, 0.116)';
+    //     this.style.background = null;
+
+    //     this.style.borderLeft = null;
+    //     this.style.borderRight = null;
+    //     this.style.boxShadow = null;
+
+    // })
+    
+//}
+
